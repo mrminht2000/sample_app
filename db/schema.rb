f37a9782cf1fd9dc3404d7621b4e0f83d2974bb8
Minh Nguyen Ngoc <mrminht2000@gmail.com>
2021-07-20 15:49:15 +0700

    Chapter 9: Advanced login
    Fix convention error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_032603) do
+ActiveRecord::Schema.define(version: 2021_07_20_080337) do
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_032603) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
